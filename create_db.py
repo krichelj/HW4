b42ee62f693b69574c4e5720607e9175a31ba81b
Shay Kricheli <skricheli2@gmail.com>
2019-01-10 21:37:56 +0200

    changes

--- create_db.py
+++ create_db.py
@@ -1,39 +1,15 @@
+import os
 import sqlite3
 import atexit
 import sys
 
-connection = sqlite3.connect('schedule.db')  # connect to the database
+data_base_existed = os.path.isfile("schedule.db")
+database_connection = sqlite3.connect("schedule.db")  # connect to the database
 
 
-# register a function to be called immediately when the interpreter terminates
-def close_db():
-    connection.commit()
-    connection.close()
-
-
-atexit.register(close_db)
-
-
-# the main() function
-def main():
-    create_tables()
-
-    with open(sys.argv[1]) as words_file:
-        word_generator = parse_words(words_file)
-        for word in word_generator:
-            print(word)
-
-    # config_file = open(sys.argv[1], 'r')
-    # for line in config_file:
-    #     line_type = line[0]
-    #     if line_type == 'C':
-    #         print(line[3])
-
-
-# our application API:
-
+# table construction function
 def create_tables():
-    connection.executescript("""
+    database_connection.executescript("""
         CREATE TABLE IF NOT EXISTS courses (
             id      INTEGER     PRIMARY KEY,
             course_name    TEXT        NOT NULL,
@@ -45,7 +21,7 @@ def create_tables():
 
         CREATE TABLE IF NOT EXISTS students (
             grade TEXT PRIMARY KEY,
-            expected_output     TEXT    NOT NULL
+            count INTEGER NOT NULL
         );
 
         CREATE TABLE IF NOT EXISTS classrooms (
@@ -58,24 +34,84 @@ def create_tables():
 
 
 def insert_course(id, course_name, student, number_of_students, class_id, course_length):
-    connection.execute("""
-          INSERT INTO courses (id, course_name, student, number_of_students, class_id, course_length) VALUES (?, ?, ?, ?, ?, ?)
+    database_connection.execute("""
+          INSERT OR REPLACE 
+          INTO courses 
+          (id, course_name, student, number_of_students, class_id, course_length) 
+          VALUES (?, ?, ?, ?, ?, ?)
     """, [id, course_name, student, number_of_students, class_id, course_length])
 
 
-def insert_student(grade, expected_output):
-    connection.execute("""
-          INSERT INTO students (grade, expected_output) VALUES (?, ?)
-    """, [grade, expected_output])
+def insert_student(grade, count):
+    database_connection.execute("""
+          INSERT OR REPLACE 
+          INTO students 
+          (grade, count) 
+          VALUES (?, ?)
+    """, [grade, count])
 
 
 def insert_classroom(id, location, current_course_id, current_course_time_left):
-    connection.execute("""
-          INSERT INTO classrooms (id, location, current_course_id, current_course_time_left) VALUES (?, ?, ?, ?)
+    database_connection.execute("""
+          INSERT OR REPLACE 
+          INTO classrooms 
+          (id, location, current_course_id, current_course_time_left) 
+          VALUES (?, ?, ?, ?)
     """, [id, location, current_course_id, current_course_time_left])
 
 
-def parse_words(config_file):
-    for line in config_file:
-        for word in line.split():
-            yield word
+def print_table(list_of_tuples):
+    for item in list_of_tuples:
+        print(item)
+
+
+def print_tables(cursor):
+    # print courses
+    print("courses")
+    cursor.execute("SELECT * FROM courses")
+    print_table(cursor.fetchall())
+    # print classrooms
+    print("classrooms")
+    cursor.execute("SELECT * FROM classrooms")
+    print_table(cursor.fetchall())
+    # print students
+    print("students")
+    cursor.execute("SELECT * FROM students")
+    print_table(cursor.fetchall())
+
+
+# register a function to be called immediately when the interpreter terminates
+def close_database():
+    database_connection.commit()
+    database_connection.close()
+
+
+atexit.register(close_database)
+
+
+# the main function
+def main():
+    if not data_base_existed:  # First time creating the database.
+        with database_connection:
+            cursor = database_connection.cursor()
+            create_tables()
+            with open(sys.argv[1], 'r') as config_file:  # parse the config file
+                for line in config_file:
+                    line = line.replace("\n", "")
+                    line_info = line.split(",")
+                    line_type = line_info[0]
+                    if line_type == 'C':  # means we're typing in a course
+                        insert_course(int(line_info[1].strip(" ")), line_info[2].strip(" "),
+                                      line_info[3].strip(" "), int(line_info[4].strip(" ")),
+                                      int(line_info[5].strip(" ")),
+                                      int(line_info[6].strip(" ")))
+                    elif line_type == 'S':  # means we're typing in a student
+                        insert_student(line_info[1].strip(" "), int(line_info[2].strip(" ")))
+                    elif line_type == 'R':  # means we're typing in a classroom
+                        insert_classroom(int(line_info[1].strip(" ")), line_info[2].strip(" "), 0, 0)
+
+            print_tables(cursor)
+
+
+if __name__ == '__main__':
+    main()
--- schedule.py
+++ schedule.py
@@ -1,3 +1,91 @@
+import os
 import sqlite3
+import create_db
 
-conn = sqlite3.connect('schedule.db')
+
+# the main function
+def main():
+    os.remove("schedule.db")
+    create_db.main()
+    data_base_existed = os.path.isfile("schedule.db")
+    database_connection = sqlite3.connect("schedule.db")  # connect to the database
+    with database_connection:
+        cursor = database_connection.cursor()
+        cursor.execute("""SELECT count(*) as tot FROM courses""")
+        courses_num = int(cursor.fetchone()[0])
+        i = 1
+        while data_base_existed and courses_num > 0:  # run as long as the database exists and there are courses left to assign
+            cursor.execute("""SELECT 
+                                    classroom.location, 
+                                    course.course_name,
+                                    course.id,
+                                    course.course_length,
+                                    classroom.id,
+                                    course.number_of_students,
+                                    course.student
+                              FROM 
+                                    classrooms as classroom 
+                              JOIN 
+                                    courses as course 
+                              ON 
+                                    classroom.id = course.class_id 
+                              AND 
+                                    classroom.current_course_time_left = 0
+                              AND
+                                    classroom.current_course_id = 0""")
+            if int(cursor.fetchone()[0]) > 0:
+                for result in cursor.fetchall():
+                    print("(" + str(i) + ") " + result[0] + ": " + result[1] + " is schedule to start")
+                    cursor.execute("""UPDATE
+                                          classrooms
+                                      SET
+                                          current_course_id = ?,
+                                          current_course_time_left = ?
+                                      WHERE
+                                          classrooms.id = ? """, [result[2], result[3], result[4]])
+                    cursor.execute("""UPDATE
+                                          students
+                                      SET
+                                          count = count - ?
+                                      WHERE
+                                          students.grade = ? """, [result[5], result[6]])
+            cursor.execute("""SELECT 
+                                  classroom.location, 
+                                  course.course_name,
+                                  classroom.id
+                              FROM 
+                                  classrooms as classroom 
+                              JOIN 
+                                  courses as course 
+                              ON 
+                                  classroom.id = course.class_id 
+                              AND 
+                                  classroom.current_course_time_left > 0""")
+            for result in cursor.fetchall():
+                print("(" + str(i) + ") " + result[0] + ": occupied by " + result[1])
+                cursor.execute("""UPDATE
+                                      classrooms
+                                  SET
+                                      current_course_time_left = current_course_time_left - 1
+                                  WHERE
+                                      classrooms.id = ? """, [result[2]])
+            cursor.execute("""SELECT 
+                                   classroom.location, 
+                                   course.course_name,
+                                   course.id
+                              FROM 
+                                   classrooms as classroom 
+                              JOIN 
+                                   courses as course 
+                              ON 
+                                   classroom.id = course.class_id 
+                              AND 
+                                   classroom.current_course_time_left = 0""")
+            for result in cursor.fetchall():
+                print("(" + str(i) + ") " + result[0] + ": " + result[1] + " is done")
+                cursor.execute("""DELETE FROM courses WHERE courses.id = ?""", [result[2]])
+            i = i + 1
+
+
+if __name__ == '__main__':
+    main()
